86f0f9124b8bfcbb597218a6b1bdebd386cc657a
Yi Xuan Sim <yxuan1996@gmail.com>
2017-10-04 12:39:12 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
     # Write your code here!
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
 
-    erb: friends
+    erb :friends
 
   end
 end
